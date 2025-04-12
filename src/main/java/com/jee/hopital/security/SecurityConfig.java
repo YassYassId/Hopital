897ea77eee4291b960d895336453998aff345809
YassYassId <idrissiy371@gmail.com>
2025-04-12 15:52:11 +0100

    Spring Security configuration

---
+++ src/main/java/com/jee/hopital/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.jee.hopital.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user").password(passwordEncoder.encode("password")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("password")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER, ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());;
+        return httpSecurity.build();
+    }
+}
